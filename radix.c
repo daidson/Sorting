6c1057e56e1b6191f94a2778227ce55f7d60c243
Daidson Alves <daidson.alves@gmail.com>
2017-06-18 17:14:21 -0300

    radix.c head created

---
+++ radix.c
@@ -0,0 +1,27 @@
+
+/***************************************************************************
+ *   radix.c                                  Version 20170618.171201      *
+ *                                                                         *
+ *   Radix Sorting Algorithm in C                                          *
+ *   Copyright (C) 2017         by Daidson Alves                           *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2 of the License.               *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Daidson Fonseca Alves                                                 * 
+ *   Email: daidson.alves@gmail.com                                        *
+ *   Webpage: https://github.com/daidson                                   *
+ *   Phone: +55 81 99954-6310                                              *
+ ***************************************************************************/
